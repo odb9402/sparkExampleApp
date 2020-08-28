b6422ad153e6c24072455a6ae814530b2c65e7d8
Dongpin <dhehdqls@gmail.com>
2020-08-28 05:37:05 +0000

    add data and update main

--- src/main/scala/RDDsampleApp.scala
+++ src/main/scala/RDDsampleApp.scala
@@ -2,17 +2,30 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 
+import scala.reflect.io.Directory
+import java.io.File
+
+
 object RDDsampleApp {
   def main(args: Array[String]) {
-        val conf = new SparkConf().setAppName("RDDsampleApp").setMaster("local")
-        val sc = new SparkContext(conf)
-        
-        //Parallelized Collections
-        val arrData = Array(1,2,3,4,5)
-        val distArrData = sc.paralleize(arrData)
-        println(s"Sum arr 1 to 5 :[${distArrData.reduce((a, b) => a + b)}]")
+    val conf = new SparkConf().setAppName("RDDsampleApp").setMaster("local")
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("FATAL")
 
-        //Using External datasets
-        val distFile = sc.textFile("sample_data.txt")
+    //Parallelized Collections
+    val arrData = Array(1,2,3,4,5)
+    val distArrData = sc.parallelize(arrData)
+    println(s"Sum arr 1 to 5 :[${distArrData.reduce((a, b) => a + b)}]")
+
+    //Using External datasets
+    val distFile = sc.textFile("./data/sample_data_*.txt") // Can use wildcard or a directory as the input
+    //val hadoopFile = sc.hadoopRDD() 
+    println(s"Total length of the file :[${distFile.map(s => s.length).reduce((a,b) => a+b)}]")
+    distFile.saveAsObjectFile("./data/object_file")
+    
+    val distFileDir = new Directory(new File("./data/object_file"))
+    distFileDir.deleteRecursively()
+  
+          
   }
 }
